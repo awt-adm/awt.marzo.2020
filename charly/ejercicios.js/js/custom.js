0643b6645809509cab3d4cde8ba7e4f199c86cff
carlosmuaz <carlos.muaz@fandeal.com.mx>
2020-04-01 19:02:57 -0600

    cambios ultima clase

---
+++ charly/ejercicios.js/js/custom.js
@@ -0,0 +1,27 @@
+// TO DO: cambiar el color del <body>
+
+document.addEventListener("DOMContentLoaded", () => {
+  let body = document.querySelector("body");
+
+  // TODO: log cuando damos click en <body/>
+  body.addEventListener("click", () => {
+    console.log("Le diste click al body");
+  });
+
+  body.style.backgroundColor = "red";
+
+  console.log("Ya cargo el documento");
+});
+
+// TO DO: Abre/Cierra una ventana modal
+// variables globales
+let botonAbreModal = document.querySelector("#mas-info button");
+let botonCierraModal = document.querySelector("#modal .contenedorModal header");
+let modal = document.querySelector("#modal");
+
+botonAbreModal.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+botonCierraModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
