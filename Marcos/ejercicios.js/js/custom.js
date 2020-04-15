76f8c8d3a74d1351a04065570fa1474d5d866c85
MarcosIslas27 <56372549+MarcosIslas27@users.noreply.github.com>
2020-04-15 18:10:33 -0500

    mi tema

--- Marcos/ejercicios.js/js/custom.js
+++ Marcos/ejercicios.js/js/custom.js
@@ -1,20 +1,22 @@
-// TODO: cambiar el color del <body>
-
-document.addEventListener("DOMContentLoaded", () => {
+/// TODO: cambiar el color del <body>
+document.addEventListener("DOMContentLoaded", () => { // callback
 
     let body = document.querySelector("body");
+
+    //DEBUG:
+    // console.log("QUE HAY EN ESTE OBJETO?::: ", body);
 
     // TODO: log cuando damos click en <body/>
     body.addEventListener('click', () => {
 
-        console.log("Le diste click al body");
+        // console.log("Le diste click al body");
 
     });
-
+    // propiedades se escriben cameCase
     body.style.backgroundColor = "red";
 
-    console.log("Ya cargo el documento");
-});
+    // console.log("Ya cargo el documento");
+}); // fin ready
 
 // TODO: Abre/Cierra una ventana modal
 // variables globales
--- Marcos/ejercicios.js/js/theme.js
+++ Marcos/ejercicios.js/js/theme.js
@@ -1,40 +1,109 @@
-document.addEventListener("DOMContentLoaded", {
+document.addEventListener("DOMContentLoaded", () => {
 
     theming();
 
+    console.log("Theming cargado!");
+}); // fin ready
 
-    console.log("Theming cargado!")
-});
+//TODO: Cambiar el fondo del body por medio de un Select desde html
+const theming = () => {
 
-//TODO: Cambiar el fondo del body por medio de un select desde html
-const theming =()=> {
-
-    let body = document.querySelector("body");
     let seleccion = document.querySelector("#select-color");
 
-    console.log(seecccion, "????");
+    // se ejecuta al cambiar el select
+    seleccion.addEventListener("change", (evento) => {
 
-    seleccion.addEventListener("change", (evento) =>{
+        cambiaColores(evento.target.value)
 
-        switch (evento. target. value) {
-            case "light":
-                body.style.backgroundColor = "whitesmoke";
-                break;
-
-            case "dark":
-                body.style.backgroundColor = "black";
-                
-                    break;
-        
-            default:
-                break;
-        }
-
-
-        console.log("Ya cambieeee", );
     });
+    // Ejecuta theme por default
+    cambiaColores(seleccion.value);
 
+}
 
-    
-    
+// Paramentros de una funcion: permiten acceder a los valores de ésta.
+const cambiaColores = (tema) => {
+
+    // variables de objetos
+    let body = document.querySelector("body");
+    let titulos = document.querySelectorAll('body h1');
+    let links = document.querySelectorAll('body a');
+    let span = document.querySelectorAll('body span');
+
+    // Variables Color light
+    let fondoLight = "#FFF"; // 
+    let tituloLight = "#012e32"; // 
+    let parrafoLight = "#000"; //
+    let linkLight = "#aa7039"; //
+    let linkHoverLight = "#842c00"; //
+    // Variables Color dark
+    let fondoDark = "#000";
+    let tituloDark = "#69969c";
+    let parrafoDark = "whitesmoke";
+    let linkDark = "#aa8d32";
+    let linkHoverDark = "#fffdf8";
+    // 
+    // Variables Color navy
+    let fondoNavy = "#00143d";
+    let tituloNavy = "#b5426e";
+    let parrafoNavy = "efb8ea";
+    let linkNavy = "#30bfc9";
+    let linkHoverNavy = "#4b5358";
+    // 
+    switch (tema) {
+        case "light":
+            //codigo
+            body.style.backgroundColor = fondoLight;
+            body.style.color = parrafoLight;
+            links.forEach((item) => { //Itera sobre un Array
+                item.style.color = linkLight;
+            });
+            titulos.forEach((item) => {
+                item.style.color = tituloLight;
+            });
+            span.forEach((item) => {
+                item.classList.add("colorTextoLight");
+                item.classList.remove("colorTextoDark", "colorTextoNavy");
+            });
+            break;
+        case "dark":
+            //codigo
+            body.style.backgroundColor = fondoDark;
+            // titulos.style.color = tituloDark;
+            body.style.color = parrafoDark;
+            links.forEach((item) => {
+                item.style.color = linkDark;
+            });
+            titulos.forEach((item) => {
+                item.style.color = tituloDark;
+
+            });
+            span.forEach((item) => {
+                item.classList.add("colorEspecialDark");
+                item.classList.remove("colorEspecialLight", "colorEspecialNavy");
+            });
+            break;
+        case "Navy":
+            //codigo
+            body.style.backgroundColor = fondoNavy;
+        
+            body.style.color = parrafoNavy;
+            links.forEach((item) => {
+                item.style.color = linkNavy;
+            });
+            titulos.forEach((item) => {
+                item.style.color = tituloNavy;
+
+            });
+            span.forEach((item) => {
+                item.classList.add("colorEspecialNavy");
+                item.classList.remove("colorEspecialLight", "colorEspecialDark");
+            });
+            break;
+        default:
+            //codigo si no se cumple el switch
+            console.log("No existe el theme");
+            break;
+    }
+
 }
